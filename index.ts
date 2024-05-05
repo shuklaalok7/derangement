5410fad2a91c46dfba36d16a65d3a6ca6653123c
shuklaalok7 <alok@clay.fish>
2024-05-05 22:05:06 +0530

    Comparing node vs bun

--- index.ts
+++ index.ts
@@ -1,11 +1,10 @@
-import bigDecimal from "js-big-decimal";
+const MINUS_ONE = BigInt(-1)
+const ONE = BigInt(1)
+const EXP = Math.exp(1)
 
-const MINUS_ONE = -1n
-const ONE = 1n
-const EXP = new bigDecimal(Math.exp(1))
 export function ithDerangement(n: number, i: number): number[] {
     const totalDerangements = subFactorial(n)
-    console.log(`totalDerangements for ${n}: ${totalDerangements}`)
+    // console.log(`totalDerangements for ${n}: ${totalDerangements}`)
 
     if (i < 1 || i > totalDerangements) {
         console.error('i is out of bounds')
@@ -32,6 +31,7 @@ export function ithDerangement(n: number, i: number): number[] {
 
             const f = frozenFreedom + (testNumber > position ? 1 : 0)
             const g = derangementCounter(n - position, f, dpForDc)
+            // const newI = jump(n - position, f, i, dpForDc)
 
             // console.log(`f = ${f} g = ${g} testNumber = ${testNumber}`)
 
@@ -51,7 +51,6 @@ export function ithDerangement(n: number, i: number): number[] {
 }
 
 const subFactorialData: { [p: number]: bigint } = {}
-
 // fixme number is running out of capacity
 export function subFactorial(n: number): bigint {
     if (n == 0)
@@ -59,7 +58,8 @@ export function subFactorial(n: number): bigint {
     if (subFactorialData[n])
         return subFactorialData[n]
     // Following function is for n>=1
-    subFactorialData[n] = BigInt(new bigDecimal(factorial(n) + BigInt(1)).divide(EXP).floor().getValue())
+    subFactorialData[n] = BigInt(Math.floor(Number(factorial(n) + ONE) / EXP))
+    // subFactorialData[n] = BigInt(new bigDecimal(factorial(n) + BigInt(1)).divide(EXP).floor().getValue())
     return subFactorialData[n]
 }
 
@@ -71,6 +71,10 @@ export function factorial(n: number, dp: { [p: number]: bigint } = {}): bigint {
     dp[n] = BigInt(n) * factorial(n - 1)
     return dp[n]
 }
+
+// export function jump(k: number, f: number, i: number, dp: bigint[][]): number {
+//
+// }
 
 /**
  *
@@ -87,8 +91,10 @@ export function derangementCounter(k: number, f: number, dp: bigint[][]): bigint
     if (f > k || f < 0) {
         throw Error('Invalid f')
     }
-    if (f == 0)
-        return subFactorial(k)
+    if (f == 0) {
+        dp[k][f] = subFactorial(k)
+        return dp[k][f]
+    }
     // Pattern found through brainstorming on a spreadsheet
     // (K2-J2*($D3-1))/(J$1-$D3+1)
     const rightUp = derangementCounter(k + 1, f - 1, dp)
@@ -99,28 +105,41 @@ export function derangementCounter(k: number, f: number, dp: bigint[][]): bigint
 
 async function main() {
     // console.log(Bun.argv)
-    let [n, i] = [0, 0]
-    if (Bun.argv.length >= 4)
-        [n, i] = [Number(Bun.argv[2]), Number(Bun.argv[3])]
-    else {
-        let input: string | null = null
+    const data = [[5, 19], [50, 1009], [50, 1009], [50, 1009]]
 
-        while (!input) {
-            input = prompt("n = ")
-        }
-        n = Number(input)
-        input = null
+    for (let datum of data) {
+        let [n, i] = datum
 
-        while (!input) {
-            input = prompt("i = ")
-        }
-        i = Number(input)
+        // while (true) {
+        // if (Bun.argv.length >= 4)
+        //     [n, i] = [Number(Bun.argv[2]), Number(Bun.argv[3])]
+        // else {
+
+        // let input: string | null = null
+        //
+        // while (!input) {
+        //     input = await inquirer.prompt(prompts[0])
+        // }
+        // if (input === 'q') {
+        //     r1.close()
+        //     break
+        // }
+        // n = Number(input)
+        // input = null
+        //
+        // while (!input) {
+        //     input = await inquirer.prompt(prompts[1])
+        // }
+        // i = Number(input)
+        // }
+        const timeLabel = `time taken for n = ${n} and i = ${i}`
+        console.time(timeLabel)
+        ithDerangement(n, i)
+        console.timeEnd(timeLabel)
+        // }
     }
-    const timeLabel = `time taken for n = ${n} and i = ${i}`
-    console.time(timeLabel)
-    ithDerangement(n, i)
-    console.timeEnd(timeLabel)
 }
 
-await main()
+main().then(() => {
+})
 
