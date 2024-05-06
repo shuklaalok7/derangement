a7c3739d5f91e6951ef39bd226de4bc0973e4563
Alok Shukla <alok@clay.fish>
2024-05-06 22:25:30 +0530

    Recursion > Loops

--- Derangement.test.ts
+++ Derangement.test.ts
@@ -1,4 +1,4 @@
-import {derangementCounter, ithDerangement} from "./index";
+import {derangementCounter} from "./index";
 import {describe, expect, test} from "bun:test";
 
 const ithDerangementTestData: [number, number, number[]][] = [
@@ -8,25 +8,34 @@ const ithDerangementTestData: [number, number, number[]][] = [
     [6, 78, [3, 4, 6, 5, 2, 1]]
 ]
 
-const derangementCounterTestData = [
-    [4, 0, 9],
-    [4, 1, 11],
-    [5, 3, 78],
-    [6, 2, 362],
-    [4, 4, 24]
+const derangementCounterTestData: [number, number, bigint][] = [
+    [4, 0, 9n],
+    [4, 1, 11n],
+    [5, 3, 78n],
+    [6, 2, 362n],
+    [4, 4, 24n]
+]
+
+const subfactorialTestData: [number, bigint][] = [
+    [0, 1n],
+    [1, 0n],
+    [2, 1n],
+    [3, 2n],
+    [4, 9n],
+    [5, 44n],
+    [13, 2290792932n]
 ]
 
 describe("tests", () => {
-    test.each(ithDerangementTestData)("ithDerangement for n = %i and i = %i should be %o", (n, i, output) => {
-        expect(ithDerangement(n, i)).toEqual(output);
+    // test.each(ithDerangementTestData)("ithDerangement for n = %i and i = %i should be %o", (n, i, output) => {
+    //     expect(ithDerangement(n, i)).toEqual(output);
+    // })
+
+    test.each(derangementCounterTestData)("DerangementCounter for k = %i and f = %i", (k, f, output) => {
+        expect(derangementCounter(k, f)).toEqual(output)
     })
 
-    test.each(derangementCounterTestData)("DerangementCounter for k = %i and f = %i should be %i", (k, f, output) => {
-        const dpForDc: number[][] = []
-        for (let j = 0; j <= k + f; j++) {
-            dpForDc[j] = new Array(f + 1).fill(-1)
-        }
-
-        expect(derangementCounter(k, f, dpForDc)).toEqual(output)
-    })
+    // test.each(subfactorialTestData)("subfactorial for n = %i", (n, output) => {
+    //     expect(subFactorial(n)).toEqual(output)
+    // })
 });
--- index.ts
+++ index.ts
@@ -1,7 +1,3 @@
-const MINUS_ONE = BigInt(-1)
-const ONE = BigInt(1)
-const EXP = Math.exp(1)
-
 export function ithDerangement(n: number, i: number): number[] {
     const totalDerangements = subFactorial(n)
     // console.log(`totalDerangements for ${n}: ${totalDerangements}`)
@@ -13,10 +9,7 @@ export function ithDerangement(n: number, i: number): number[] {
 
     const output: number[] = new Array(n)
     const used: boolean[] = new Array(n)
-    const dpForDc: bigint[][] = []
-    for (let j = 0; j <= n + 1; j++) {
-        dpForDc[j] = new Array(n + 1).fill(MINUS_ONE)
-    }
+
     for (let position = 1; position <= n; position++) {
         let frozenFreedom = 0
         for (let i = 1; i < position; i++) {
@@ -30,7 +23,7 @@ export function ithDerangement(n: number, i: number): number[] {
                 continue
 
             const f = frozenFreedom + (testNumber > position ? 1 : 0)
-            const g = derangementCounter(n - position, f, dpForDc)
+            const g = derangementCounter(n - position, f)
             // const newI = jump(n - position, f, i, dpForDc)
 
             // console.log(`f = ${f} g = ${g} testNumber = ${testNumber}`)
@@ -50,94 +43,143 @@ export function ithDerangement(n: number, i: number): number[] {
     return output
 }
 
-const subFactorialData: { [p: number]: bigint } = {}
-// fixme number is running out of capacity
+const subFactorialCache: { [p: number]: bigint } = {}
+
+// fixme Running into stack overflow
 export function subFactorial(n: number): bigint {
-    if (n == 0)
-        return ONE
-    if (subFactorialData[n])
-        return subFactorialData[n]
-    // Following function is for n>=1
-    subFactorialData[n] = BigInt(Math.floor(Number(factorial(n) + ONE) / EXP))
-    // subFactorialData[n] = BigInt(new bigDecimal(factorial(n) + BigInt(1)).divide(EXP).floor().getValue())
-    return subFactorialData[n]
+    if (n === 0) return 1n;   // Base case: !0 = 1
+    if (n === 1) return 0n;   // Base case: !1 = 0
+
+    if (subFactorialCache[n])
+        return subFactorialCache[n]
+
+    // Initialize an array for storing subfactorial results
+    subFactorialCache[0] = 1n; // !0
+    subFactorialCache[1] = 0n; // !1
+
+    // Compute subfactorial using the DP table
+    for (let i = 2; i <= n; i++) {
+        subFactorialCache[i] = BigInt(i - 1) * (subFactorialCache[i - 1] + subFactorialCache[i - 2]);
+    }
+
+    return subFactorialCache[n];
 }
 
-export function factorial(n: number, dp: { [p: number]: bigint } = {}): bigint {
-    if (n == 1 || n == 0)
-        return ONE
-    if (dp[n])
-        return dp[n]
-    dp[n] = BigInt(n) * factorial(n - 1)
-    return dp[n]
+const dcCache: { [k: number]: { [f: number]: bigint } } = {}
+
+
+/**
+ * Calculates derangement values iteratively using dynamic programming.
+ * @param maxK The maximum k value to compute values for.
+ * @param maxF The maximum f value to compute values for.
+ */
+function fillDerangementTable(maxK: number, maxF: number): void {
+    for (let f = 0; f <= maxF; f++) {
+        for (let k = maxK + maxF - f; k >= maxK; k--) {
+            if (f > k)
+                break
+
+            if (!(k in dcCache))
+                dcCache[k] = {}
+            else if (f in dcCache[k]) {
+                console.log(`dcCache[${k}][${f}] = ${(dcCache[k][f]).toString()}`)
+                continue
+            }
+
+            if (f == 0) {
+                dcCache[k][f] = subFactorial(k);
+                console.log(`dcCache[${k}][${f}] = ${(dcCache[k][f]).toString()}`)
+            } else {
+                const rightUp = dcCache[k + 1][f - 1]
+                const up = dcCache[k][f - 1]
+                dcCache[k][f] = (rightUp - up * BigInt(f - 1)) / BigInt(k - f + 1);
+                console.log(`dcCache[${k}][${f}] = ${(dcCache[k][f]).toString()}`)
+            }
+        }
+    }
 }
 
-// export function jump(k: number, f: number, i: number, dp: bigint[][]): number {
-//
-// }
+/**
+ * Uses a precomputed table to return the derangement value.
+ * @param k
+ * @param f
+ */
+export function derangementCounter(k: number, f: number): bigint {
+    if (k in dcCache && f in dcCache[k]) {
+        return dcCache[k][f]
+    }
+    fillDerangementTable(k, f)  // Ensuring the cache is filled up to at least (k, f)
+    return dcCache[k][f]
+}
+
 
 /**
  *
  * @param k
  * @param f
- * @param dp Must be at least (k+f)*(f+1) size matrix of derangementCounts. Row is k and column is f.
  */
-export function derangementCounter(k: number, f: number, dp: bigint[][]): bigint {
-    // console.log(`dp = ${JSON.stringify(dp)}\nk = ${k}\t f = ${f}`)
-    // console.log(`dp[${k}][${f}] = ${dp[k][f]}`)
-    if (dp[k][f] != MINUS_ONE)
-        return dp[k][f]
-
-    if (f > k || f < 0) {
-        throw Error('Invalid f')
-    }
-    if (f == 0) {
-        dp[k][f] = subFactorial(k)
-        return dp[k][f]
-    }
-    // Pattern found through brainstorming on a spreadsheet
-    // (K2-J2*($D3-1))/(J$1-$D3+1)
-    const rightUp = derangementCounter(k + 1, f - 1, dp)
-    const up = derangementCounter(k, f - 1, dp)
-    dp[k][f] = (rightUp - up * BigInt(f - 1)) / BigInt(k - f + 1)
-    return dp[k][f]
-}
+// export function derangementCounter(k: number, f: number): bigint {
+//     if (k in dcCache && f in dcCache[k])
+//         return dcCache[k][f]
+//
+//     if (f > k || f < 0) {
+//         throw Error('Invalid f')
+//     }
+//
+//     if (!dcCache[k])
+//         dcCache[k] = {}
+//
+//     if (f == 0) {
+//         dcCache[k][f] = subFactorial(k)
+//         return dcCache[k][f]
+//     }
+//     const rightUp = derangementCounter(k + 1, f - 1)
+//     const up = derangementCounter(k, f - 1)
+//     dcCache[k][f] = (rightUp - up * BigInt(f - 1)) / BigInt(k - f + 1)
+//     return dcCache[k][f]
+// }
 
 async function main() {
     // console.log(Bun.argv)
-    const data = [[5, 19], [50, 1009], [50, 1009], [50, 1009]]
+    const data = [2, 3, 6, 50, 50, 50, 50];
 
     for (let datum of data) {
-        let [n, i] = datum
-
-        // while (true) {
-        // if (Bun.argv.length >= 4)
-        //     [n, i] = [Number(Bun.argv[2]), Number(Bun.argv[3])]
-        // else {
-
-        // let input: string | null = null
-        //
-        // while (!input) {
-        //     input = await inquirer.prompt(prompts[0])
-        // }
-        // if (input === 'q') {
-        //     r1.close()
-        //     break
-        // }
-        // n = Number(input)
-        // input = null
-        //
-        // while (!input) {
-        //     input = await inquirer.prompt(prompts[1])
-        // }
-        // i = Number(input)
-        // }
-        const timeLabel = `time taken for n = ${n} and i = ${i}`
-        console.time(timeLabel)
-        ithDerangement(n, i)
-        console.timeEnd(timeLabel)
-        // }
+        console.time('subfactorial time')
+        subFactorial(datum)
+        console.timeEnd('subfactorial time')
     }
+
+    // // for (let datum of data) {
+    //     let [n, i] = [0,1]
+    //
+    //     while (true) {
+    //         if (Bun.argv.length >= 4)
+    //             [n, i] = [Number(Bun.argv[2]), Number(Bun.argv[3])]
+    //         else {
+    //             let input: string | null = null
+    //
+    //             while (!input) {
+    //                 input = prompt("n = ")
+    //             }
+    //
+    //             n = Number(input)
+    //             input = null
+    //
+    //             while (!input) {
+    //                 input = prompt("i = ")
+    //             }
+    //             i = Number(input)
+    //         }
+    //         if (n < 1 || i < 1) {
+    //             console.warn('n and i should be greater than 0. Exiting...')
+    //             break
+    //         }
+    //         const timeLabel = `time taken for n = ${n} and i = ${i}`
+    //         console.time(timeLabel)
+    //         ithDerangement(n, i)
+    //         console.timeEnd(timeLabel)
+    //     }
+    // }
 }
 
 main().then(() => {
